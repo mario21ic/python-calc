a27a6d63c6ec01d0be3dbeb41df64d7843a00e23
Mario Inga <mario21ic@gmail.com>
2021-07-14 16:44:56 -0500

    adding sleep 2

--- docker-test.sh
+++ docker-test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set +e
 
+docker rm -f pycalc-demo
 docker run -d --rm --name pycalc-demo -p 8081:8081 mario21ic/pycalc:v1.0
 
 sleep 5
 
 curl localhost:8081
 
-docker rm pycalc-demo
+docker rm -f pycalc-demo
 
